a40c56946ae72a0b4324787ee857fa3bfff0bf7b
tom-lv <tomlv.dev@gmail.com>
2022-09-01 14:22:34 +0100

    Add project files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Header from './components/sections/Header';
+import About from './components/sections/About';
+import Education from './components/sections/Education';
+import Projects from './components/sections/Projects';
+import Contact from './components/sections/Contact';
+import Footer from './components/sections/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <About />
+      <Education />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/sections/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      {/* Heading */}
+      <h1 className='text-center'>Tomas Le Vesconte</h1>
+
+      {/* Bio */}
+      <p className='text-center'>
+        Aspiring 
+        <code className='font-bold'> Software Engineer </code>
+        & 
+        <code className='font-bold'> CompSci Graduate</code>
+      </p>
+      <p className='text-center'>from South Wales, United Kingdom.</p>
+
+      {/* Icons */}
+    </header>
+  )
+}
+
+export default Header
