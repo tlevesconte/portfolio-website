a9d021ae6e5ae478286079b59b5cbad85012b25d
tom-lv <tomlv.dev@gmail.com>
2022-09-02 15:13:09 +0100

    Add comments & small changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Canvas } from '@react-three/fiber';
 
 import Header from './components/sections/Header';
 import About from './components/sections/About';
@@ -6,12 +7,25 @@ import Education from './components/sections/Education';
 import Projects from './components/sections/Projects';
 import Contact from './components/sections/Contact';
 import Footer from './components/sections/Footer';
+import AsciiTorus from './3D/AsciiTorus';
 
 function App() {
   return (
     <div className='App'>
+      {/* Canvas object */}
+      <div className='h-[500px]'>
+        <Canvas>
+          <AsciiTorus />
+        </Canvas>
+      </div>
+
+      {/* Header */}
+      <Header />
+
+      {/* Container */}
       <div className='static max-w-4xl mx-auto'>
-        <Header />
+        
+        {/* Sections */}
         <About />
         <Education />
         <Projects />
