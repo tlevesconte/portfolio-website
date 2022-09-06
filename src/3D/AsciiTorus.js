dd2c54ffc2c41ff9315f15937acae0cd08b8b3a8
tom-lv <tomlv.dev@gmail.com>
2022-09-06 23:45:15 +0100

    Add ascii torus feature

--- src/3D/AsciiTorus.js
+++ src/3D/AsciiTorus.js
@@ -1,5 +1,35 @@
-import React, { useRef } from 'react'
-import { useFrame } from '@react-three/fiber'
+import React, { useRef, useMemo, useEffect } from 'react'
+import { useFrame, useThree } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei';
+import { AsciiEffect } from 'three-stdlib';
+
+const AsciiRenderer = ({ renderIndex = 1, characters = ' .:-+*=%@#', ...options }) => {
+  const { gl, size, scene, camera } = useThree();
+  
+  const effect = useMemo(() => {
+    const effect = new AsciiEffect(gl, characters, options)
+    effect.domElement.style.position = 'absolute';
+    effect.domElement.style.top = '0px';
+    effect.domElement.style.left = '0px';
+    effect.domElement.style.color = 'white';
+    effect.domElement.style.backgroundColor = '#111111';
+    effect.domElement.style.pointerEvents = 'none';
+    return effect;
+  }, [characters, options.invert]);
+
+  useEffect(() => {
+    gl.domElement.parentNode.appendChild(effect.domElement)
+    return () => gl.domElement.parentNode.removeChild(effect.domElement)
+  }, [effect]);
+
+  useEffect(() => {
+    effect.setSize(size.width, size.height)
+  }, [effect, size]);
+
+  useFrame((state) => {
+    effect.render(scene, camera)
+  }, renderIndex);
+}
 
 const AsciiTorus = () => {
   const meshRef = useRef();
@@ -9,15 +39,18 @@ const AsciiTorus = () => {
       return;
     }
 
+    // Animation
     // meshRef.current.rotation.x += 0.004;
-    meshRef.current.rotation.y += 0.004;
+    // meshRef.current.rotation.y += 0.004;
     // meshRef.current.rotation.z += 0.0001;
   });
 
   return (
     <mesh ref={meshRef}>
-        <torusGeometry args={[1.4, 1, 30, 100]}/>
-        <meshStandardMaterial color="black" wireframe/>
+        <torusGeometry args={[1.4, 1, 30, 100]} />
+        <meshStandardMaterial color='white' />
+        <OrbitControls />
+        <AsciiRenderer invert />
     </mesh>
   );
 }
--- src/App.js
+++ src/App.js
@@ -8,16 +8,21 @@ import Projects from './components/sections/Projects';
 import Contact from './components/sections/Contact';
 import Footer from './components/sections/Footer';
 import AsciiTorus from './3D/AsciiTorus';
+import { PerspectiveCamera } from '@react-three/drei';
 
 function App() {
   return (
     <div className='App'>
-      {/* Canvas object */}
-      <div className='h-[500px]'>
+      <div className='canvas h-screen w-screen bg-[#111111]'></div>
+      {/* Canvas object (AsciiTorus)*/}
+      {/* <div className='canvas h-[350px]'>
         <Canvas>
+          <color attach="background" args={['black']} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <pointLight position={[-10, -10, -10]} />
           <AsciiTorus />
         </Canvas>
-      </div>
+      </div> */}
 
       {/* Header */}
       <Header />
