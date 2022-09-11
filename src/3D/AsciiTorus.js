d9e0e839c795dad83d912f6591c650e9eb279234
tom-lv <tomlv.dev@gmail.com>
2022-09-11 13:22:40 +0100

    Remove AsciiTorus.js from App.js

--- src/3D/AsciiTorus.js
+++ src/3D/AsciiTorus.js
@@ -48,11 +48,21 @@ const AsciiTorus = () => {
   return (
     <mesh ref={meshRef}>
         <torusGeometry args={[1.4, 1, 30, 100]} />
-        <meshStandardMaterial color='white' />
+        <meshStandardMaterial />
         <OrbitControls />
         <AsciiRenderer invert />
     </mesh>
   );
 }
+
+{/* Canvas object (AsciiTorus)*/}
+        {/* <div className='canvas h-screen w-screen'>
+        <Canvas>
+          <color attach="background" args={['black']} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <pointLight position={[-10, -10, -10]} />
+          <AsciiTorus />
+        </Canvas>
+        </div> */}
 
 export default AsciiTorus;
