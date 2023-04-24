28bef8be15466442db0ab672aebad95155732049
Tomas L <tomlv.dev@gmail.com>
2023-04-24 22:28:01 +0100

    Set default text size to text-base (#12)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ const montserrat = Montserrat({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${montserrat.className} text-[#999] bg-[#191A1F]`}>
+    <main className={`${montserrat.className} text-[#999] bg-[#191A1F] text-base`}>
       <Component {...pageProps} />
     </main>
   );
