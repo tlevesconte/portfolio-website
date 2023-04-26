0e0dcdc3b1197d126362eb9c6f75a6748b110843
Tomas L <tomlv.dev@gmail.com>
2023-04-26 17:40:48 +0100

    Change font configuration (#18)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,12 @@ import { Montserrat } from 'next/font/google';
 
 const montserrat = Montserrat({
   subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
+  variable: '--font-montserrat',
 });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${montserrat.className} text-[#999] bg-[#191A1F] text-sm sm:text-[0.9375rem] sm:leading-{1.375rem}`}>
+    <main className={`${montserrat.variable} font-montserrat sm:leading-{1.375rem} bg-[#191A1F] text-sm text-[#999] sm:text-[0.9375rem]`}>
       <Component {...pageProps} />
     </main>
   );
