5bb22f30c512f6bfafbbb5aa02c9b528c3a4dcf9
tlevesconte <tomaslv.dev@gmail.com>
2023-10-17 10:48:22 +0200

    Change <p> text

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -9,14 +9,14 @@ export default function Header() {
         <div className="flex flex-col justify-center text-[1.2em]">
           <h1 className="mb-[.8em] text-[1.6em] font-bold leading-[normal]">Hi! I'm Tomas.</h1>
           <p className="leading-[1.5] text-paragraphColour">
-            I'm a Software Engineer with dual British and Swedish nationality, currently residing in
-            Sweden. If you'd like to reach out, feel free to contact me via email at {" "}
+            I'm a Software Engineer with dual British and Swedish nationality living in
+            Sweden. If you'd like to get in touch, you can reach me at {" "}
             <a
               className="border-b border-dotted border-black text-highlightColour hover:bg-[#EBEBEB]"
               href="mailto:tomas.levesconte@gmail.com">
               tomas.levesconte@gmail.com
             </a>
-            . You can also connect with me on{" "}
+            . You can also find me on {" "}
             <a className="border-b border-dotted border-black text-highlightColour hover:bg-[#EBEBEB]">
               GitHub
             </a>{" "}
