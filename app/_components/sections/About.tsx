9f45f7a4dcc611f8bc85811a289c0d8d6a532241
tlevesconte <tomaslv.dev@gmail.com>
2024-02-08 14:41:34 +0100

    Change about me

--- app/_components/sections/About.tsx
+++ app/_components/sections/About.tsx
@@ -5,19 +5,9 @@ export default function About() {
     <section className="mb-[5em] lg:scroll-mt-24" id="about">
       <div className="text-[1.2em] leading-[1.5] text-white/60">
         <p className="mb-[.8em]">
-          Hi! I&apos;m Tomas, an aspiring Software Engineer living in Sweden.
-          Passionate about web development, I thrive on challenging projects and
-          love embracing new technologies.
-        </p>
-        {/* Possible replace keeping physically fit for running */}
-        <p className="mb-[.8em]">
-          In my spare time, I like to keep physically fit and set personal
-          challenges for myself, which at the moment involves a lot of
-          long-distance running.
-          {/* P.S.: I have both British and Swedish citizenship. */}
-        </p>
-        <p className="mb-[.8em]">
-          If you&apos;d like to get in touch, you can reach me at{" "}
+          Hi! I&apos;m Tomas, an aspiring Software Engineer living in Sweden. In
+          my spare time, I enjoy running and working on personal projects. If
+          you&apos;d like to get in touch, you can reach me at{" "}
           <a
             className="border-b border-dotted border-white text-white hover:bg-white/10"
             href="mailto:tomas.levesconte@gmail.com"
